bbe856f334491fb137b7a727a308232d480d1f3b
smpr <gcbrown1116@gmail.com>
2017-12-01 01:52:54 -0500

    signin page wired up still needs tokens

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import Test from './components/Test/TestPage'
+
+import HomePage from './components/HomePage/HomePage'
 
 class App extends Component {
+  state = {
+    email: '',
+    password: '',
+    password_confirmation: ''
+}
+
+signUp = (event) => {
+    event.preventDefault()
+    this.props.signUp(
+        this.state.email,
+        this.state.password,
+        this.state.password_confirmation
+    )
+}
+
+signIn = (event) => {
+    event.preventDefault()
+    this.props.signIn(
+        this.state.email,
+        this.state.password
+    )
+}
+
+handleChange = (event) => {
+    const newState = {...this.state}
+    newState[event.target.name] = event.target.value
+    this.setState(newState)
+}
   render() {
+    const SignUpLogInComponent = () => (
+      <HomePage
+        signUp={this.signUp}
+        signIn={this.signIn}
+        signOut={this.signOut}
+        loggedIn={this.state.signedIn} />
+    )
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      
+        <Router>
+         
+
+           
+
+            <Switch>
+
+              <Route exact path="/" render={SignUpLogInComponent}/>
+
+            </Switch>
+          
+        </Router>
+      
     );
   }
 }
